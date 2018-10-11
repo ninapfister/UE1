6734c97544fe4cb9f93e51e4e8176b12c349ada6
Pfister Nina <1810653169@stud.fh-kufstein.ac.at>
2018-10-11 11:12:46 +0200

    Nachname+Geburstjahr

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String nachname = "Pfister";
+        int geburtsjahr = (int) 1997;
+        System.out.println("Hello "+nachname+" du bist im Jahr "+geburtsjahr+" geboren");
+    }
+}
