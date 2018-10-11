8abb80b553ee18c5514eb21a2af13943d528113d
Pfister Nina <1810653169@stud.fh-kufstein.ac.at>
2018-10-11 11:30:54 +0200

    Alter+Geburtsjahr

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String vorname = "Nina";
-        String nachname= "Pfister";
-        System.out.println("Hallo mein Name ist "+vorname+" "+nachname);
+        int alter = 21;
+        int geburtsjahr= 1997;
+        System.out.println("Hallo ich bin "+alter +" Jahre alt und ich bin "+geburtsjahr +" geboren");
     }
 }
